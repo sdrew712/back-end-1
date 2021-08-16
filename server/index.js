65628bf6a6012d0eade9f9cabf05e93fb6519ed3
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-16 13:31:58 -0700

    finished assignment

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/users", (req, res) => {
+  const friends = ["Zack", "Dillon", "Gabby", "Chelsea"];
+  res.status(200).send(friends);
+})
+
+app.get("/weather/:temperature", (req, res) => {
+  const { temperature } = req.params;
+  const phrase = `<h3>It was ${temperature} today</h3>`;
+  res.status(200).send(phrase);
+})
+
+app.listen(4000, () => {
+  console.log("We are running on port 4000");
+})
